674d339e9fa162723262be08730226a2b820f9fd
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-31 00:03:44 +0800

    [Jeffery] build parking system backend basic stack

---
+++ src/main/java/com/oocl/overwatcher/ParkingSysApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.overwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSysApplication.class, args);
+	}
+}
