1774d88c00d2ce911045de6291d6209eb3f98cac
LIULE8 <scau_lzc@163.com>
2018-08-01 15:03:36 +0800

    add ParkingLotController and parkingLotService and parkingLotRepository 's updateStatusById

---
+++ src/main/java/com/oocl/overwatcher/controller/ParkingLotController.java
@@ -0,0 +1,50 @@
+package com.oocl.overwatcher.controller;
+
+import com.oocl.overwatcher.entities.ParkingLot;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.oocl.overwatcher.service.ParkingLotService;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @author LIULE9
+ */
+@RestController
+@RequestMapping("/parkingLots")
+public class ParkingLotController {
+
+    @Autowired
+    private ParkingLotService parkingLotService;
+
+    @GetMapping
+    public ResponseEntity<List<ParkingLot>> findAll() {
+        return ResponseEntity.ok(parkingLotService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addParkingLot(@NotNull @RequestBody ParkingLot parkingLot) {
+        try {
+            parkingLotService.save(parkingLot);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/status")
+    public ResponseEntity<Void> updateParkingLotStatus(@RequestBody ParkingLot parkingLot) {
+        if (StringUtils.isNotBlank(parkingLot.getStatus()) && StringUtils.isNotBlank(parkingLot.getId() + "")) {
+            parkingLotService.updateStatus(parkingLot);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+
+}
--- src/main/java/com/oocl/overwatcher/repositories/ParkingLotRepository.java
+++ src/main/java/com/oocl/overwatcher/repositories/ParkingLotRepository.java
@@ -2,6 +2,15 @@ package com.oocl.overwatcher.repositories;
 
 import com.oocl.overwatcher.entities.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ParkingLotRepository extends JpaRepository<ParkingLot,Long> {
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+
+    @Query(value = "update Parking_Lot set `status` = ?2 where id = ?1", nativeQuery = true)
+    @Modifying
+    void updateStatusById( Long id, String status);
 }
