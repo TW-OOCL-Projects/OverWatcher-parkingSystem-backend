496b1fae145e961a424b82404e796e6e2a0de727
ZHOUAD <Jing690518>
2018-08-01 11:49:22 +0800

    orders

---
+++ src/main/java/com/oocl/overwatcher/controller/OrdersController.java
@@ -0,0 +1,33 @@
+package com.oocl.overwatcher.controller;
+
+import com.oocl.overwatcher.entities.Orders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.OrdersService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrdersController {
+
+    private final OrdersService ordersService;
+
+    @Autowired
+    public OrdersController(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
+
+    @GetMapping
+    public HttpEntity<List<Orders>> getOrders() {
+        return new ResponseEntity<>(ordersService.getOrders(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public List<Orders> addOrders(@RequestBody Orders orders){
+        return ordersService.addOrders(orders);
+    }
+}
---
+++ src/main/java/com/oocl/overwatcher/entities/Orders.java
@@ -0,0 +1,61 @@
+package com.oocl.overwatcher.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @JsonIgnore
+//    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+
+    private String type;
+    private String status;
+    private String carId;
+
+    public Orders() {
+    }
+
+    public Orders(String type, String status, String carId) {
+        this.type=type;
+        this.status=status;
+        this.carId=carId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+}
