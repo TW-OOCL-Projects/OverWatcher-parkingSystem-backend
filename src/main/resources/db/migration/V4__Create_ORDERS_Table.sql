f54346d40f772be683e7d7bc0bd5567bf7fa8aa5
ZHOUAD <Jing690518>
2018-08-01 13:46:22 +0800

    update V4__Create_ORDERS_Table.sql

--- src/main/resources/db/migration/V4__Create_ORDERS_Table.sql
+++ src/main/resources/db/migration/V4__Create_ORDERS_Table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `orders` (
   `id`             INT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `uesr_id`        INT,
+  `user_id`        INT,
   `type`           VARCHAR(20)  NOT NULL,
-  `stauts`         VARCHAR(20)  NOT NULL,
+  `status`         VARCHAR(20)  NOT NULL,
   `car_id`          VARCHAR(20)  NOT NULL
-
  );
