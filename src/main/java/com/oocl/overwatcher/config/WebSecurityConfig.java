3b80fb94bab1b3241df15f56221521f3f42e0acb
LIULE8 <scau_lzc@163.com>
2018-08-01 17:22:49 +0800

    open users and orders api

--- src/main/java/com/oocl/overwatcher/config/WebSecurityConfig.java
+++ src/main/java/com/oocl/overwatcher/config/WebSecurityConfig.java
@@ -57,6 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //允许所有用户访问首页 与 登录
                 .antMatchers("/", "/auth/login").permitAll()
                 .antMatchers("/parkingLots").permitAll()
+                .antMatchers("/orders").permitAll()
+                .antMatchers("/users").permitAll()
                 //其它任何请求都要经过认证通过
                 .anyRequest().authenticated()
                 //用户页面需要用户权限
