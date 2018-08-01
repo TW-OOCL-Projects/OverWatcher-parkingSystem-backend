4bc9fc2cc7787b968ca29956f70425ed986e03ae
BaleLin <bale.lin@oocl.com>
2018-08-01 18:04:33 +0800

    Bale,Quinn:update the employeeDto and the query User

---
+++ src/main/java/com/oocl/overwatcher/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.oocl.overwatcher.controller;
+
+import com.oocl.overwatcher.dto.EmployeeDto;
+import com.oocl.overwatcher.entities.User;
+import com.oocl.overwatcher.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<EmployeeDto> findAllUser(){
+        List<EmployeeDto> employeeDtos=userService.findAllUser().stream().map(user -> new EmployeeDto(user)).collect(Collectors.toList());
+        return employeeDtos;
+    }
+}
---
+++ src/main/java/com/oocl/overwatcher/service/UserService.java
@@ -0,0 +1,17 @@
+package com.oocl.overwatcher.service;
+
+import com.oocl.overwatcher.entities.User;
+import com.oocl.overwatcher.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> findAllUser(){
+        return userRepository.findAll();
+    }
+}
