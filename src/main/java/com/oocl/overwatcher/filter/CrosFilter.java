d1074e370f1fd1716cac74cc06228d035f2f85f2
BaleLin <bale.lin@oocl.com>
2018-08-01 10:49:00 +0800

    Bale,Leo：添加跨域的过滤器

---
+++ src/main/java/com/oocl/overwatcher/filter/CrosFilter.java
@@ -0,0 +1,38 @@
+package com.oocl.overwatcher.filter;
+
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * @author LIULE9
+ */
+@Order(1)
+@WebFilter(filterName = "crosFilter", urlPatterns = "/*")
+public class CrosFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        String origin = (String) servletRequest.getRemoteHost() + ":" + servletRequest.getRemotePort();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE,PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
