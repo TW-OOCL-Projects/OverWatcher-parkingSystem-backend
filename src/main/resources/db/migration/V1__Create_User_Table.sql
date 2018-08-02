a28fe930531ddcc6512bb6c109f3cf764eb0bff6
TANGTR2 <tracy.tang@oocl.com>
2018-08-03 00:35:57 +0800

    alter db phonedata

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -9,14 +9,14 @@ CREATE TABLE `User` (
 );
 
 -- username=wer password=war
-INSERT INTO `User` VALUES (1, '小明','wer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (2, '小红','dsa', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (3, '小刚','dsa', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (4, '小亮','wfdsfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (5, '小新','wfdfser', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (6, '小烘','wefdsfr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (7, '小干','wefdsfsr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (8, '小配','wfdser', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (9, '小了','wfsdfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (10, '小时','wfsdfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
-INSERT INTO `User` VALUES (11, '小发','wefsdr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','1213545');
+INSERT INTO `User` VALUES (1, '小明','wer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (2, '小红','dsa', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (3, '小刚','dsa', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (4, '小亮','wfdsfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (5, '小新','wfdfser', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (6, '小烘','wefdsfr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (7, '小干','wefdsfsr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (8, '小配','wfdser', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (9, '小了','wfsdfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (10, '小时','wfsdfer', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
+INSERT INTO `User` VALUES (11, '小发','wefsdr', '$2a$10$EgmhRAlC5iw.KeENZ5.RYebSEHTcO8yl9lOf/r36GoyGb4uNOOhr2','alive','544097676@qq.com','13011112222');
