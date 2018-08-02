fa4edbe5588a6c8cd2ccdc8474a18eb68e4e5a8e
ZHOUAD <Jing690518>
2018-08-02 22:38:37 +0800

    orders API

--- src/main/java/com/oocl/overwatcher/repositories/OrdersRepository.java
+++ src/main/java/com/oocl/overwatcher/repositories/OrdersRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders,Integer> {
 
-    @Query(value = "select * from orders where car_id = ?1 and ststus!='取车完成'", nativeQuery = true)
+    @Query(value = "select * from orders where car_id = ?1 and status<>'取车完成'", nativeQuery = true)
     Orders findBycarId(String carId);
 
     @Query(value = "update orders set `status` = ?2 where id = ?1", nativeQuery = true)
