d27badc1921efc9f3f167d21740905db5ce160ca
ZHOUAD <Jing690518>
2018-08-03 21:42:57 +0800

    orders API

--- src/main/java/com/oocl/overwatcher/controller/OrdersController.java
+++ src/main/java/com/oocl/overwatcher/controller/OrdersController.java
@@ -153,7 +153,7 @@ public class OrdersController {
         return ordersService.getParkingLotId(id);
     }
     //根据停车员ID查看历史订单
-    @GetMapping("/ordersHistory/{userId}")
+    @GetMapping("/orders/{userId}")
     public List<Orders> getHistoryByUserId(@PathVariable Long userId){
         return ordersService.getHistoryByUserId(userId);
     }
