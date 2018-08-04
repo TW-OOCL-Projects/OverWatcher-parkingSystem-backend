36bfedcdf860e14e591ff95c284d428d2fcab749
LIULE8 <scau_lzc@163.com>
2018-08-04 10:03:25 +0800

    update WebSecurityConfig and add find parkingboys by condition api

--- src/main/java/com/oocl/overwatcher/config/WebSecurityConfig.java
+++ src/main/java/com/oocl/overwatcher/config/WebSecurityConfig.java
@@ -57,12 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //允许所有用户访问首页 与 登录
                 .antMatchers("/", "/auth/login").permitAll()
                 .antMatchers("/parkingLots/**").permitAll()
-                .antMatchers("/orders").permitAll()
+                .antMatchers("/parkingBoys/**").permitAll()
                 .antMatchers("/orders/**").permitAll()
                 .antMatchers("/employees/**").permitAll()
-                .antMatchers("/employees").permitAll()
-                .antMatchers("/orders/after/*").permitAll()
-                .antMatchers("/parkingBoy/parkingLots/*").permitAll()
+                .antMatchers("/parkingBoy/**").permitAll()
                 //其它任何请求都要经过认证通过
                 .anyRequest().authenticated()
                 //用户页面需要用户权限
--- src/main/java/com/oocl/overwatcher/controller/RoleController.java
+++ src/main/java/com/oocl/overwatcher/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.oocl.overwatcher.controller;
 import com.oocl.overwatcher.dto.EmployeeDto;
 import com.oocl.overwatcher.entities.User;
 import com.oocl.overwatcher.service.RoleService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class RoleController {
     @Autowired
     private RoleService roleService;
+
     @GetMapping("/parkingBoys")
-    public List<EmployeeDto> findAllParkingBoys(){
+    public List<EmployeeDto> findAllParkingBoys() {
         List<User> parkingBoys = new ArrayList<>();
         roleService.findAllParkingBoys().forEach(role -> parkingBoys.addAll(role.getUsers()));
         return parkingBoys.stream().map(EmployeeDto::new).collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/parkingBoys/condition")
+    public List<EmployeeDto> findAllParkingBoysByCondition(String condition, String value) {
+        List<User> parkingBoys = new ArrayList<>();
+        roleService.findAllParkingBoys().forEach(role -> parkingBoys.addAll(role.getUsers()));
+        if (StringUtils.isNotBlank(condition) && "name".equals(condition)) {
+            return parkingBoys.stream().filter(parkingBoy -> parkingBoy.getName().contains(value)).map(EmployeeDto::new).collect(Collectors.toList());
+        } else if (StringUtils.isNotBlank(condition) && "email".equals(condition)) {
+            return parkingBoys.stream().filter(parkingBoy -> parkingBoy.getEmail().contains(value)).map(EmployeeDto::new).collect(Collectors.toList());
+        } else if (StringUtils.isNotBlank(condition) && "phone".equals(condition)) {
+            return parkingBoys.stream().filter(parkingBoy -> parkingBoy.getPhone().contains(value)).map(EmployeeDto::new).collect(Collectors.toList());
+        } else if (StringUtils.isNotBlank(condition) && "status".equals(condition)) {
+            return parkingBoys.stream().filter(parkingBoy -> parkingBoy.getStatus().contains(value)).map(EmployeeDto::new).collect(Collectors.toList());
+        }
+       return parkingBoys.stream().map(EmployeeDto::new).collect(Collectors.toList());
     }
 }
